bdd5f17299984b68b474f29214aa93499dc27743
nikolalsvk <nikolaseap@gmail.com>
2017-09-04 14:40:19 +0000

    Second stop of the tutorial

---
+++ src/hello-ewe/hello-ewe.component.spec.ts
@@ -0,0 +1,34 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {HelloEweComponent} from './hello-ewe.component';
+
+describe('HelloEweComponent', () => {
+  let component: HelloEweComponent;
+  let fixture: ComponentFixture<HelloEweComponent>;
+  let dom: any;
+
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        declarations: [HelloEweComponent],
+      });
+      fixture = TestBed.createComponent(HelloEweComponent);
+      component = fixture.componentInstance;
+
+      dom = fixture.nativeElement;
+    }),
+  );
+
+  test('should exist', () => {
+    expect(component).toBeDefined();
+  });
+
+  test('should have a default name', () => {
+    expect(component.name).toBe('Matt');
+  });
+
+  test('should output a <p> with "Baaaa, {{ name }}!"', () => {
+    fixture.detectChanges(); // renders the dom
+    expect(dom.innerHTML).toBe('<p>Baaaa, Matt!</p>');
+  });
+});
